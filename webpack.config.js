fb0c53903ef3a1e6f0e0b711e213f6dd69e1da36
VI <vika.miroshnikova2016@gmail.com>
2024-07-07 18:02:48 +0300

    Удалены не нужные строки

--- webpack.config.js
+++ webpack.config.js
@@ -7,23 +7,17 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
 
 
+
 module.exports = {
+
     entry: {
         main: path.resolve(__dirname, './src/index.js'),
     },
-}
-
-module.exports = {
-    // ...
 
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js',
     },
-}
-
-
-module.exports = {
 
     mode: 'development',
     devServer: {
