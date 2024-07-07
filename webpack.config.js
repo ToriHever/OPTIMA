57e845253aad0fca99014aae1bf6a1b9c6630d2d
VI <vika.miroshnikova2016@gmail.com>
2024-07-07 16:44:20 +0300

    Установлен Babel (JavaScript)

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,17 @@ module.exports = {
         }),
         new CleanWebpackPlugin(),
     ],
+
+    module: {
+        rules: [
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    }
 }
 
 
