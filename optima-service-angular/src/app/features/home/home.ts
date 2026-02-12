3111599f6b7bbd7b03960f9485e96f3e7376c44e
VI <vika.miroshnikova2016@gmail.com>
2026-02-12 23:03:24 +0300

    Добавлен процесс работы

--- optima-service-angular/src/app/features/home/home.ts
+++ optima-service-angular/src/app/features/home/home.ts
@@ -3,11 +3,12 @@ import { Hero } from './components/hero/hero';
 import { BrandsCarousel } from './components/brands-carousel/brands-carousel';
 import { ServicesTable } from './components/services-table/services-table';
 import { RepairCategories } from './components/repair-categories/repair-categories';
+import { WorkProcess } from './components/work-process/work-process';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Hero, BrandsCarousel, RepairCategories, ServicesTable],
+  imports: [Hero, BrandsCarousel, RepairCategories, ServicesTable, WorkProcess],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
