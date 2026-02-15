49e3b99b9e4fa67ffa5e37b8871c0000566f0169
VI <vika.miroshnikova2016@gmail.com>
2026-02-15 21:03:43 +0300

    Измена анимация появления модалки

--- optima-service-angular/src/app/shared/components/callback-modal/callback-modal.ts
+++ optima-service-angular/src/app/shared/components/callback-modal/callback-modal.ts
@@ -26,13 +26,14 @@ import { CALLBACK_FORM_CONFIGS } from './callback-form.config';
     ]),
     trigger('slideIn', [
       transition(':enter', [
-        style({ transform: 'translateY(-50px)', opacity: 0 }),
-        animate('300ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
+        style({ opacity: 0 }),
+        animate('300ms ease-out', style({ opacity: 1 }))
       ]),
       transition(':leave', [
         animate('200ms ease-in', style({ transform: 'translateY(-50px)', opacity: 0 }))
       ])
-    ])
+    ]
+  )
   ]
 })
 export class CallbackModal implements OnInit, OnDestroy {
