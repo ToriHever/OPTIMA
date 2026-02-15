36b203cd44615b29407bd21016bf1dac468f549d
VI <vika.miroshnikova2016@gmail.com>
2026-02-15 21:19:54 +0300

    Измена анимация появления модалки

--- optima-service-angular/src/app/shared/components/repair-status-modal/repair-status-modal.ts
+++ optima-service-angular/src/app/shared/components/repair-status-modal/repair-status-modal.ts
@@ -33,8 +33,8 @@ interface RepairInfo {
     ]),
     trigger('slideIn', [
       transition(':enter', [
-        style({ opacity: 0, transform: 'translateY(-50px)' }),
-        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+        style({ opacity: 0 }),
+        animate('300ms ease-out', style({ opacity: 1}))
       ]),
       transition(':leave', [
         animate('200ms ease-in', style({ opacity: 0, transform: 'translateY(-50px)' }))
