5d53e7fbcf92a542b6223c996abff230aa345313
VI <vika.miroshnikova2016@gmail.com>
2026-02-17 20:42:16 +0300

    Панель социальных сетей

--- optima-service-angular/src/app/app.ts
+++ optima-service-angular/src/app/app.ts
@@ -4,11 +4,12 @@ import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { RepairStatusModal } from './shared/components/repair-status-modal/repair-status-modal';
 import { CallbackModal } from './shared/components/callback-modal/callback-modal';
+import { SocialBar } from './layout/social-bar/social-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer, RepairStatusModal, CallbackModal],
+  imports: [RouterOutlet, Header, Footer, RepairStatusModal, CallbackModal, SocialBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ optima-service-angular/src/app/layout/social-bar/social-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-social-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './social-bar.html',
+  styleUrl: './social-bar.scss'
+})
+export class SocialBar {}
